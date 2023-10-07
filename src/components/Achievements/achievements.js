bab2046f358f7a35f3ef285cd0622009ffb8de2b
aryan-kanwar <a2kanwar@uwaterloo.ca>
2023-10-06 23:42:43 -0400

    make clickable

--- src/components/Achievements/achievements.js
+++ src/components/Achievements/achievements.js
@@ -10,23 +10,29 @@ const Achievements = () => {
       <div className="achievement-row">
         {achievements &&
           achievements.map((achievement) => (
-            <div
-              className="my-achievement"
+            <a
+              href={achievement.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="clickable-achievement-box"
               key={achievement.id}
-              data-aos="zoom-in-up"
-              data-aos-duration="1500"
             >
-              <h4 className="achievement-name">
-                {achievement.achievement_name}
-              </h4>
-              <p className="achievement-desc">
-                {achievement.achievement_description}
-              </p>
-            </div>
+              <div
+                className="my-achievement"
+                data-aos="zoom-in-up"
+                data-aos-duration="1500"
+              >
+                <h4 className="achievement-name">
+                  {achievement.achievement_name}
+                </h4>
+                <p className="achievement-desc">
+                  {achievement.achievement_description}
+                </p>
+              </div>
+            </a>
           ))}
       </div>
     </section>
   );
 };
-
 export default Achievements;
--- src/data/achievements.js
+++ src/data/achievements.js
@@ -4,16 +4,22 @@ export const achievements = [
     achievement_name: "Distributed-Fault-Tolerant-Map-Reduce",
     achievement_description:
       "Developed A fault-tolerant Map-Reduce system featuring a master-worker architecture with dynamic task reassignment and worker recovery",
+      link: "https://github.com/aryankanwar/Distributed-Fault-Tolerant-Map-Reduce" // Add the link for the first achievement
+
   },
   {
     id: 2,
     achievement_name: "NoteKeep",
     achievement_description:
       "Notekeep is a sleek and user-friendly web application built on the MongoDB, Express, React, and Node (MERN) stack. It is designed to simplify and enhance your note-taking experience.",
+      link: "https://github.com/aryankanwar/Notekeep" // Add the link for the first achievement
+
   },
   {
     id: 3,
     achievement_name: "NewsDeck",
     achievement_description: "NewsDeck is a single-page news application developed using React.The app boasts a range of features, offering news from various categories, such as general, business, entertainment, health, science, sports, and technology.",
+    link: "https://github.com/aryankanwar/News-Deck" // Add the link for the first achievement
+
   },
 ];
